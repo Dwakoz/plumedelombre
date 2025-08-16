baad19f46e9c3d69d81de43eeb31609ffc37be48
Dwakoz <kevin.blanc.47@gmail.com>
2025-08-16 10:07:15 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,5 @@
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("styles.css");
-  eleventyConfig.addFilter("excerpt", (content) => {
-    if(!content) return "";
-    const txt = content.replace(/<[^>]*>/g, ' ').replace(/\s+/g, ' ').trim();
-    return txt.split(' ').slice(0,70).join(' ') + (txt.split(' ').length>70?'…':''); 
-  });
-  return {
-    dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    },
-    templateFormats: ["njk", "html", "md"]
-  };
-}
+  return { dir:{ input:".", includes:"_includes", data:"_data", output:"_site" } };
+};

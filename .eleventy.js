385bb2d44c9b9ff9d84291a94eb707ecf912e778
Dwakoz <kevin.blanc.47@gmail.com>
2025-08-16 10:21:12 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("styles.css");
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/decap-cms/dist/decap-cms.js": "admin/decap-cms.js"
+  });
   return { dir:{ input:".", includes:"_includes", data:"_data", output:"_site" } };
 };

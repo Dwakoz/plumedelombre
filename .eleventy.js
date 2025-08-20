e223afba22255bbbedf5b2e51db8d653904f9718
Dwakoz <kevin.blanc.47@gmail.com>
2025-08-20 02:24:00 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,18 @@
 module.exports = function (eleventyConfig) {
+  // Assets
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy({ "static": "uploads" });
   eleventyConfig.addPassthroughCopy({ "dashboard": "dashboard" });
 
+  // Collections
   eleventyConfig.addCollection("textes", (c) =>
     c.getFilteredByGlob("content/textes/**/*.md").sort((a, b) => b.date - a.date)
   );
   eleventyConfig.addCollection("articles", (c) =>
     c.getFilteredByGlob("content/articles/**/*.md").sort((a, b) => b.date - a.date)
+  );
+  eleventyConfig.addCollection("fiction", (c) =>
+    c.getFilteredByGlob("content/fiction/**/*.md").sort((a, b) => b.date - a.date)
   );
 
   return {
